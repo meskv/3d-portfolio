dfb9b622ecc84ab7d6ecfe0708c82f4dbe5a5435
meskv <krsk1495@gmail.com>
2023-06-25 01:14:40 +0530

    fix: now footer visible in light mode

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
         <>
-            <div className=" min-h-[4rem] py-2  flex justify-center items-center gap-4 bg-transparent">
+            <div className=" min-h-[4rem] py-2 flex justify-center items-center gap-4 bg-white dark:bg-transparent">
                 {
                     socialLinks.map((social, index) => (
-                        <Link key={social} to={social.link} target="_blank" className='text-4xl cursor-pointer'>
+                        <Link key={social} to={social.link} target="_blank" className='text-4xl text-gray-800 dark:text-gray-200 cursor-pointer'>
                             <social.icon />
                         </Link>
                     ))
